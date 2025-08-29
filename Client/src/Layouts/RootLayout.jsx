d7c72ea503d6852d7eaee4c2be337976fe16a866
mdshakibsami <mdshakibsami21@gmail.com>
2025-08-29 16:21:02 +0600

    implement react router

--- Client/src/Layouts/RootLayout.jsx
+++ Client/src/Layouts/RootLayout.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { Outlet } from "react-router";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 const RootLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
